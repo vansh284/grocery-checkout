4fb023630fcd88fc1a3440bb3b9347203ac09a92
mzozozn <3142962678@qq.com>
2026-04-05 21:42:20 +0200

    Update mainpage.h

--- mainpage.h
+++ mainpage.h
@@ -4,27 +4,31 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include "page.h"
+#include "categorypage.h"
 
 class MainPage : public Page
 {
     Q_OBJECT
 public:
     explicit MainPage(CartData *cart, QWidget *parent = nullptr);
+    void refreshUI();
 
 signals:
     void checkoutConfirmed();
+    void categoryRequested(const QString &categoryName);
+
+public slots:
+    void addWeightedItem(const CategoryPage::WeightedItem &item, double weightKg);
 
 private:
-    QVBoxLayout *m_itemsList;
-    QLabel *m_subtotalLabel;
+    QVBoxLayout *m_itemsList = nullptr;
+    QLabel *m_subtotalLabel = nullptr;
+
     void addItem(const QString &name, int qty, double price);
 
 private slots:
     void onPayClicked();
     void showBagDialog();
-
-public:
-    void refreshUI();
 };
 
 #endif // MAINPAGE_H
