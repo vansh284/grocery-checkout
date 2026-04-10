2d5f53f592989c3f935a1fab2e8652358196b0a7
mzozozn <3142962678@qq.com>
2026-04-10 07:48:30 +0200

    Adjust layout

--- numpaddialog.cpp
+++ numpaddialog.cpp
@@ -1,342 +1,238 @@
-#include "numpaddialog.h"
+#include "mainwindow.h"
+
+#include "welcomepage.h"
+#include "mainpage.h"
+#include "paymentpage.h"
+#include "receiptpage.h"
+#include "adminpage.h"
+#include "categorypage.h"
+#include "itemdetailpage.h"
+#include "helppage.h"
+
+#include <QMenu>
+#include <QMenuBar>
+#include <QStatusBar>
+
+#include <QDialog>
 #include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QGridLayout>
+#include <QFormLayout>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QDoubleSpinBox>
+#include <QDialogButtonBox>
 #include <QLabel>
-#include <QPushButton>
-#include <QFrame>
-#include <QFont>
-#include <QPainter>
-#include <QPainterPath>
 
-class BackspaceButton : public QPushButton {
-public:
-    BackspaceButton(QWidget* parent = nullptr) : QPushButton("✖", parent) {
-        setFixedSize(60, 50);
-        setFont(QFont("Arial", 20, QFont::Bold));
-        setCursor(Qt::PointingHandCursor);
-    }
-
-protected:
-    void paintEvent(QPaintEvent* event) override {
-        // 不调用基类的 paintEvent，我们完全自己画
-        Q_UNUSED(event);
-        QPainter painter(this);
-        painter.setRenderHint(QPainter::Antialiasing); // 开启抗锯齿，边缘更平滑
-
-        int w = width();
-        int h = height();
-        int arrowWidth = 15; // 左侧尖角的宽度（你可以自己调）
-
-        // 定义多边形的 5 个顶点
-        QPolygon polygon;
-        polygon << QPoint(w, 0)                  // 右上角
-                << QPoint(w, h)                  // 右下角
-                << QPoint(arrowWidth, h)         // 左下折点
-                << QPoint(0, h / 2)              // 最左边尖角 (垂直居中)
-                << QPoint(arrowWidth, 0);        // 左上折点
-
-        // 根据鼠标按下/松开的状态，设置不同的红色
-        QColor bgColor = isDown() ? QColor("#B5403C") : QColor("#D45450");
-        painter.setPen(Qt::NoPen);   // 不需要边框线
-        painter.setBrush(bgColor);   // 填充颜色
-
-        // 绘制多边形背景
-        painter.drawPolygon(polygon);
-
-        // 绘制中间的 "✖" 文字
-        painter.setPen(Qt::white);
-        painter.drawText(rect(), Qt::AlignCenter, text());
-    }
-};
-
-NumpadDialog::NumpadDialog(Mode mode, QWidget *parent)
-    : QDialog(parent),
-    m_mode(mode),
-    m_barcodeDisplay(nullptr),
-    m_euroDisplay(nullptr),
-    m_centDisplay(nullptr)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
 {
-    setWindowFlags(Qt::Dialog | Qt::FramelessWindowHint);
-    setAttribute(Qt::WA_TranslucentBackground);
-    setFixedSize(420, 580); // 稍微加宽一点点以容纳价格模式的多个控件
+    statusBar()->hide();
+    QMenu *systemMenu = menuBar()->addMenu("Simulation");
 
-    QFrame *mainFrame = new QFrame(this);
-    mainFrame->setFixedSize(420, 580);
-    mainFrame->setStyleSheet(
-        "QFrame {"
-        "  background-color: #FAF2D4;"
-        "  border: 3px solid black;"
-        "  border-radius: 16px;"
-        "}"
-        );
+    QAction *scanAction = systemMenu->addAction("Scan");
+    QAction *adminAction = systemMenu->addAction("Admin Mode");
 
-    QVBoxLayout *mainLayout = new QVBoxLayout(mainFrame);
-    mainLayout->setContentsMargins(30, 30, 30, 30);
-    mainLayout->setSpacing(20);
+    setWindowTitle("Self-Checkout System");
+    resize(1280, 768);
 
-    // 1. 标题
-    QLabel *titleLabel = new QLabel();
-    titleLabel->setFont(QFont("Arial", 28));
-    titleLabel->setAlignment(Qt::AlignCenter);
-    titleLabel->setStyleSheet("border: none;");
-    if (m_mode == BarcodeMode) {
-        titleLabel->setText("Enter barcode");
-    } else {
-        titleLabel->setText("Price");
-    }
-    mainLayout->addWidget(titleLabel);
+    m_cartData = new CartData();
 
-    // 2. 顶部输入区域
-    QHBoxLayout *inputLayout = new QHBoxLayout();
+    m_stack = new QStackedWidget(this);
+    setCentralWidget(m_stack);
 
-    // 退格键 (两个模式通用，靠右侧)
-    BackspaceButton *backspaceBtn = new BackspaceButton();
-    connect(backspaceBtn, &QPushButton::clicked, this, &NumpadDialog::onBackspaceClicked);
+    m_welcomePage    = new WelcomePage(m_cartData, this);
+    m_mainPage       = new MainPage(m_cartData, this);
+    m_paymentPage    = new PaymentPage(m_cartData, this);
+    m_receiptPage    = new ReceiptPage(m_cartData, this);
+    m_adminPage      = new AdminPage(m_cartData, this);
+    m_categoryPage   = new CategoryPage(m_cartData, this);
+    m_itemDetailPage = new ItemDetailPage(m_cartData, this);
+    m_helpPage       = new HelpPage(m_cartData, this);
 
-    if (m_mode == BarcodeMode) {
-        // --- 条码模式的输入框 ---
-        m_barcodeDisplay = new QLineEdit();
-        m_barcodeDisplay->setFixedHeight(60);
-        m_barcodeDisplay->setFont(QFont("Arial", 24));
-        m_barcodeDisplay->setReadOnly(true);
-        m_activeField = FieldBarcode; // 永远激活
+    m_stack->insertWidget(PAGE_WELCOME,     m_welcomePage);
+    m_stack->insertWidget(PAGE_MAIN,        m_mainPage);
+    m_stack->insertWidget(PAGE_PAYMENT,     m_paymentPage);
+    m_stack->insertWidget(PAGE_RECEIPT,     m_receiptPage);
+    m_stack->insertWidget(PAGE_ADMIN,       m_adminPage);
+    m_stack->insertWidget(PAGE_CATEGORY,    m_categoryPage);
+    m_stack->insertWidget(PAGE_ITEM_DETAIL, m_itemDetailPage);
+    m_stack->insertWidget(PAGE_HELP,        m_helpPage);
 
-        inputLayout->addWidget(m_barcodeDisplay);
-        inputLayout->addWidget(backspaceBtn);
+    connect(m_welcomePage, &WelcomePage::languageSelected,
+            this, &MainWindow::onLanguageSelected);
 
-        setActiveField(FieldBarcode); // 设置紫边样式
+    connect(m_mainPage,       &Page::backClicked, this, &MainWindow::onBackClicked);
+    connect(m_paymentPage,    &Page::backClicked, this, &MainWindow::onBackClicked);
+    connect(m_receiptPage,    &Page::backClicked, this, &MainWindow::onBackClicked);
+    connect(m_categoryPage,   &Page::backClicked, this, &MainWindow::onBackClicked);
+    connect(m_itemDetailPage, &Page::backClicked, this, &MainWindow::onBackClicked);
+    connect(m_helpPage,       &Page::backClicked, this, &MainWindow::onBackClicked);
 
-    } else {
-        // --- 价格模式的输入框 ---
-        inputLayout->setAlignment(Qt::AlignCenter);
+    connect(m_mainPage,       &Page::helpClicked, this, [this]() { navigateTo(PAGE_HELP); });
+    connect(m_paymentPage,    &Page::helpClicked, this, [this]() { navigateTo(PAGE_HELP); });
+    connect(m_receiptPage,    &Page::helpClicked, this, [this]() { navigateTo(PAGE_HELP); });
+    connect(m_categoryPage,   &Page::helpClicked, this, [this]() { navigateTo(PAGE_HELP); });
+    connect(m_itemDetailPage, &Page::helpClicked, this, [this]() { navigateTo(PAGE_HELP); });
+    connect(m_adminPage,      &Page::helpClicked, this, [this]() { navigateTo(PAGE_HELP); });
 
-        // 创建一个“画板”容器，用来固定里面的绝对坐标
-        QWidget *priceContainer = new QWidget();
-        priceContainer->setFixedSize(290, 60);
+    connect(m_mainPage, &MainPage::categoryRequested, this, [this](const QString &categoryName) {
+        m_currentCategory = categoryName;
+        m_currentCategoryItems = CategoryPage::itemsForCategory(categoryName);
+        m_categoryPage->setCategory(m_currentCategory, m_currentCategoryItems, m_currentWeightKg);
+        navigateTo(PAGE_CATEGORY);
+    });
 
-        m_euroDisplay = new QLineEdit("0", priceContainer);
-        m_euroDisplay->setFixedSize(80, 60);
-        m_euroDisplay->setFont(QFont("Arial", 24, QFont::Bold));
-        m_euroDisplay->setAlignment(Qt::AlignCenter);
-        m_euroDisplay->setReadOnly(true);
-        // 安装事件过滤器以捕获点击
-        m_euroDisplay->installEventFilter(this);
-        m_euroDisplay->setGeometry(85, 0, 80, 60);
-
-        QLabel *dotLabel = new QLabel(".", priceContainer);
-        dotLabel->setFont(QFont("Arial", 32, QFont::Bold));
-        dotLabel->setStyleSheet("border: none"); // 让小数点靠下一点
-        dotLabel->setGeometry(165, 10, 20, 50);
-
-        m_centDisplay = new QLineEdit("00", priceContainer);
-        m_centDisplay->setFixedSize(70, 60);
-        m_centDisplay->setFont(QFont("Arial", 24, QFont::Bold));
-        m_centDisplay->setAlignment(Qt::AlignCenter);
-        m_centDisplay->setReadOnly(true);
-        // 安装事件过滤器
-        m_centDisplay->installEventFilter(this);
-        m_centDisplay->setGeometry(180, 0, 70, 60);
-
-        QLabel *euroSignLabel = new QLabel("€", priceContainer);
-        euroSignLabel->setFont(QFont("Arial", 24, QFont::Bold));
-        euroSignLabel->setStyleSheet("border: none");
-        euroSignLabel->setGeometry(260, 0, 40, 60);
-
-        inputLayout->addWidget(priceContainer);
-        inputLayout->addSpacing(15);
-        inputLayout->addWidget(backspaceBtn);
-
-        // 默认选中元
-        setActiveField(FieldEuros);
-    }
-
-    mainLayout->addLayout(inputLayout);
-
-    // 3. 数字键盘网格 (完全复用)
-    QGridLayout *gridLayout = new QGridLayout();
-    gridLayout->setSpacing(15);
-
-    QString numBtnStyle =
-        "QPushButton {"
-        "  background-color: #F1D413;"
-        "  color: black;"
-        "  border: 3px solid black;"
-        "  border-radius: 12px;"
-        "}"
-        "QPushButton:pressed { background-color: #D6BC0F; }";
-
-    for (int i = 0; i < 9; ++i) {
-        int row = i / 3;
-        int col = i % 3;
-        QPushButton *btn = new QPushButton(QString::number(i + 1));
-        btn->setFixedSize(90, 90);
-        btn->setFont(QFont("Arial", 28, QFont::Bold));
-        btn->setStyleSheet(numBtnStyle);
-        connect(btn, &QPushButton::clicked, this, &NumpadDialog::onNumberClicked);
-        gridLayout->addWidget(btn, row, col);
-    }
-
-    QPushButton *cancelBtn = new QPushButton("✖");
-    cancelBtn->setFixedSize(90, 90);
-    cancelBtn->setFont(QFont("Arial", 40, QFont::Bold));
-    cancelBtn->setStyleSheet(
-        "QPushButton {"
-        "  background-color: #C92A2A;"
-        "  color: white;"
-        "  border: 3px solid black;"
-        "  border-radius: 12px;"
-        "}"
-        "QPushButton:pressed { background-color: #A32222; }"
-        );
-    connect(cancelBtn, &QPushButton::clicked, this, &NumpadDialog::onCancelClicked);
-
-    QPushButton *zeroBtn = new QPushButton("0");
-    zeroBtn->setFixedSize(90, 90);
-    zeroBtn->setFont(QFont("Arial", 28, QFont::Bold));
-    zeroBtn->setStyleSheet(numBtnStyle);
-    connect(zeroBtn, &QPushButton::clicked, this, &NumpadDialog::onNumberClicked);
-
-    QPushButton *confirmBtn = new QPushButton("✔");
-    confirmBtn->setFixedSize(90, 90);
-    confirmBtn->setFont(QFont("Arial", 40, QFont::Bold));
-    confirmBtn->setStyleSheet(
-        "QPushButton {"
-        "  background-color: #A4D156;"
-        "  color: white;"
-        "  border: 3px solid black;"
-        "  border-radius: 12px;"
-        "}"
-        "QPushButton:pressed { background-color: #8BB247; }"
-        );
-    connect(confirmBtn, &QPushButton::clicked, this, &NumpadDialog::onConfirmClicked);
-
-    gridLayout->addWidget(cancelBtn, 3, 0);
-    gridLayout->addWidget(zeroBtn, 3, 1);
-    gridLayout->addWidget(confirmBtn, 3, 2);
-
-    mainLayout->addLayout(gridLayout);
-}
-
-// 样式更新控制中枢
-void NumpadDialog::setActiveField(ActiveField field) {
-    m_activeField = field;
-
-    // 基础的 QLineEdit 样式
-    QString baseStyle = "background-color: white; border-radius: 8px; color: #666666;";
-
-    if (m_mode == BarcodeMode && m_barcodeDisplay) {
-        m_barcodeDisplay->setStyleSheet("QLineEdit {" + baseStyle + " border: 3px solid #5C32E6; padding: 0 10px; }");
-    }
-    else if (m_mode == PriceMode) {
-        QString activeBorder = "border: 4px solid black;";
-        QString inactiveBorder = "border: 2px solid black;";
-
-        if (field == FieldEuros) {
-            m_euroDisplay->setStyleSheet("QLineEdit {" + baseStyle + activeBorder + "}");
-            m_centDisplay->setStyleSheet("QLineEdit {" + baseStyle + inactiveBorder + "}");
-        } else if (field == FieldCents) {
-            m_euroDisplay->setStyleSheet("QLineEdit {" + baseStyle + inactiveBorder + "}");
-            m_centDisplay->setStyleSheet("QLineEdit {" + baseStyle + activeBorder + "}");
+    connect(m_categoryPage, &CategoryPage::itemSelected, this, [this](const CategoryPage::WeightedItem &item) {
+        if (!m_categoryPage->hasValidWeight()) {
+            return;
         }
-    }
-}
 
-// 事件过滤器拦截点击以切换选中框
-bool NumpadDialog::eventFilter(QObject *watched, QEvent *event) {
-    if (m_mode == PriceMode && event->type() == QEvent::MouseButtonPress) {
-        if (watched == m_euroDisplay) {
-            setActiveField(FieldEuros);
-            return true;
-        } else if (watched == m_centDisplay) {
-            setActiveField(FieldCents);
-            return true;
+        m_currentWeightKg = m_categoryPage->currentWeightKg();
+        m_itemDetailPage->setItemDetail(m_currentCategory, item, m_currentWeightKg);
+        navigateTo(PAGE_ITEM_DETAIL);
+    });
+
+    connect(m_itemDetailPage, &ItemDetailPage::addItemRequested, this,
+            [this](const CategoryPage::WeightedItem &item, double weightKg) {
+                m_mainPage->addWeightedItem(item, weightKg);
+                navigateTo(PAGE_MAIN);
+            });
+
+    connect(m_mainPage, &MainPage::checkoutConfirmed, this, [this]() {
+        navigateTo(PAGE_PAYMENT);
+    });
+
+    connect(m_paymentPage, &PaymentPage::cashSelected, this, [this]() {
+        navigateTo(PAGE_RECEIPT);
+    });
+
+    connect(m_paymentPage, &PaymentPage::cardSelected, this, [this]() {
+        navigateTo(PAGE_RECEIPT);
+    });
+
+    connect(m_receiptPage, &ReceiptPage::finished, this, [this]() {
+        resetToWelcome();
+    });
+
+    connect(scanAction, &QAction::triggered, this, [this]() {
+        QDialog dialog(this);
+        dialog.setWindowTitle("Simulate Scan");
+        dialog.setModal(true);
+        dialog.setFixedWidth(420);
+
+        QVBoxLayout *root = new QVBoxLayout(&dialog);
+        root->setContentsMargins(20, 20, 20, 20);
+        root->setSpacing(16);
+
+        QLabel *title = new QLabel("Add scanned item");
+        title->setFont(QFont("Arial", 18, QFont::Bold));
+        title->setStyleSheet("color: #111111;");
+        title->setAlignment(Qt::AlignCenter);
+        root->addWidget(title);
+
+        QFormLayout *form = new QFormLayout();
+        form->setSpacing(12);
+        form->setLabelAlignment(Qt::AlignLeft);
+
+        QLineEdit *nameEdit = new QLineEdit();
+        nameEdit->setPlaceholderText("Item name");
+        nameEdit->setText("Sample Item");
+
+        QSpinBox *qtySpin = new QSpinBox();
+        qtySpin->setRange(1, 999);
+        qtySpin->setValue(1);
+
+        QDoubleSpinBox *priceSpin = new QDoubleSpinBox();
+        priceSpin->setRange(0.01, 99999.99);
+        priceSpin->setDecimals(2);
+        priceSpin->setPrefix("€");
+        priceSpin->setSingleStep(0.10);
+        priceSpin->setValue(1.00);
+
+        form->addRow("Item name:", nameEdit);
+        form->addRow("Quantity:", qtySpin);
+        form->addRow("Unit price:", priceSpin);
+
+        root->addLayout(form);
+
+        QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+        root->addWidget(buttonBox);
+
+        connect(buttonBox, &QDialogButtonBox::accepted, &dialog, [&]() {
+            const QString itemName = nameEdit->text().trimmed();
+            const int qty = qtySpin->value();
+            const double price = priceSpin->value();
+
+            if (itemName.isEmpty()) {
+                nameEdit->setFocus();
+                return;
+            }
+
+            m_mainPage->addItem(itemName, qty, price);
+
+            if (m_stack->currentIndex() != PAGE_MAIN) {
+                navigateTo(PAGE_MAIN);
+            }
+
+            dialog.accept();
+        });
+
+        connect(buttonBox, &QDialogButtonBox::rejected, &dialog, &QDialog::reject);
+
+        dialog.exec();
+    });
+
+    connect(adminAction, &QAction::triggered, this, [this]() {
+        if (m_stack->currentIndex() != PAGE_ADMIN) {
+            m_adminPage->loadCart();
+            navigateTo(PAGE_ADMIN);
         }
-    }
-    return QDialog::eventFilter(watched, event);
+    });
+
+    connect(m_adminPage, &AdminPage::logoutClicked, this, [this]() {
+        m_mainPage->loadCart();
+        onBackClicked();
+    });
+
+    m_stack->setCurrentIndex(PAGE_WELCOME);
 }
 
-QString NumpadDialog::getBarcode() const {
-    return m_barcodeDisplay ? m_barcodeDisplay->text() : "";
+MainWindow::~MainWindow()
+{
 }
 
-double NumpadDialog::getPrice() const {
-    if (m_mode == PriceMode) {
-        QString euros = m_euroDisplay->text().isEmpty() ? "0" : m_euroDisplay->text();
-        QString cents = m_centDisplay->text().isEmpty() ? "00" : m_centDisplay->text();
-        QString priceStr = euros + "." + cents;
-        return priceStr.toDouble();
-    }
-    return 0.0;
+void MainWindow::onLanguageSelected(const QString &langCode)
+{
+    m_cartData->clear();
+    m_cartData->language = langCode;
+
+    m_mainPage->updateLanguageFlag(langCode);
+    m_paymentPage->updateLanguageFlag(langCode);
+    m_receiptPage->updateLanguageFlag(langCode);
+    m_adminPage->updateLanguageFlag(langCode);
+    m_categoryPage->updateLanguageFlag(langCode);
+    m_itemDetailPage->updateLanguageFlag(langCode);
+    m_helpPage->updateLanguageFlag(langCode);
+
+    m_mainPage->refreshUI();
+    navigateTo(PAGE_MAIN);
 }
 
-void NumpadDialog::onNumberClicked() {
-    QPushButton *btn = qobject_cast<QPushButton*>(sender());
-    if (!btn) return;
-    QString val = btn->text();
-
-    if (m_activeField == FieldBarcode) {
-        m_barcodeDisplay->setText(m_barcodeDisplay->text() + val);
-    }
-    else if (m_activeField == FieldEuros) {
-        QString t = m_euroDisplay->text();
-        if (t == "0") t = ""; // 消除默认值
-        if (t.length() < 3) {
-            m_euroDisplay->setText(t + val);
-        }
-    }
-    else if (m_activeField == FieldCents) {
-        QString currentCents = m_centDisplay->text();
-        // 提取当前的第二位（即右边的数字），它将变成第一位（左边的数字）
-        QString newFirstChar = currentCents.right(1);
-        // 组合新的两位数：原来的右边位 + 新输入的数字
-        QString newCents = newFirstChar + val;
-
-        m_centDisplay->setText(newCents);
-    }
+void MainWindow::navigateTo(int pageIndex)
+{
+    m_pageHistory.push(m_stack->currentIndex());
+    m_stack->setCurrentIndex(pageIndex);
 }
 
-void NumpadDialog::onBackspaceClicked() {
-    if (m_activeField == FieldBarcode) {
-        QString t = m_barcodeDisplay->text();
-        if (!t.isEmpty()) {
-            t.chop(1);
-            m_barcodeDisplay->setText(t);
-        }
-    }
-    else if (m_activeField == FieldEuros) {
-        QString t = m_euroDisplay->text();
-        if (t != "0") {
-            t.chop(1);
-            if (t.isEmpty()) t = "0"; // 恢复默认值
-            m_euroDisplay->setText(t);
-        }
-    }
-    else if (m_activeField == FieldCents) {
-        QString currentCents = m_centDisplay->text();
-
-        // 如果已经是 00，就不做任何操作
-        if (currentCents == "00") return;
-
-        // 提取当前的第一位（即左边的数字），它将退回到第二位（右边的数字）
-        QString oldFirstChar = currentCents.left(1);
-
-        // 左边补 0，右边是原来左边的数字
-        QString newCents = "0" + oldFirstChar;
-
-        m_centDisplay->setText(newCents);
-        // =========================
+void MainWindow::onBackClicked()
+{
+    if (!m_pageHistory.isEmpty()) {
+        int previousPage = m_pageHistory.pop();
+        m_stack->setCurrentIndex(previousPage);
     }
 }
 
-void NumpadDialog::onCancelClicked() {
-    reject();
-}
-
-void NumpadDialog::onConfirmClicked() {
-    // 条码模式不能为空，价格模式则默认都有数字所以可以直接确认
-    if (m_mode == BarcodeMode && m_barcodeDisplay->text().isEmpty()) {
-        return;
-    }
-    accept();
+void MainWindow::resetToWelcome()
+{
+    m_pageHistory.clear();
+    m_stack->setCurrentIndex(PAGE_WELCOME);
 }
